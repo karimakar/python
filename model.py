ed00a0c90489bb22e2be1764051a75882cbd1f31
karim-akar <35411138+karimakar@users.noreply.github.com>
2023-03-11 00:21:25 +0100

    Add files via upload

---
+++ model.py
@@ -0,0 +1,20 @@
+  # create first function
+
+def doc():
+    print('hello every one in my models') 
+# function for sum
+def sum(a,b):
+    print(f'sum of  {a} and {b} is',a+b)
+def product(a,b):
+    print(f' {a} *  {b} = ',a*b)  
+
+person = {
+'name':'karim',
+'age':30,
+'hobby':'collection of meteorite',
+'gender':'male'
+}          
+
+
+
+
---
+++ test.py
@@ -0,0 +1,24 @@
+import model
+print('the result of this porduct is:')
+model.product(10,4)
+
+
+# read some data in our jason object
+a = model.person['age']
+b = model.person['hobby']
+print(a)
+print(b)
+
+import platform
+print('my system is:')
+y  =platform.system()
+print(y)
+
+# using dir function
+u = dir(platform)
+print('the dir of our system:')
+print(u)
+    
+
+
+
